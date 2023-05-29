6374f979fa15c0393010048c07f771fa79ed345e
Varun Sharma <varun@deepfence.io>
2023-05-29 05:19:24 +0000

    v2-handle-stuck-job: Using atomic bool

--- scan/scan_context.go
+++ scan/scan_context.go
@@ -1,12 +1,16 @@
 package scan
 
+import "sync/atomic"
+
 type ScanContext struct {
 	ScanID         string
-	Aborted        bool
+	Aborted        atomic.Bool
 	ScanStatusChan chan bool
 }
 
 func NewScanContext(scanID string) *ScanContext {
 	statusChan := make(chan bool)
-	return &ScanContext{scanID, false, statusChan}
+	obj := ScanContext{scanID, atomic.Bool{}, statusChan}
+	obj.Aborted.Store(false)
+	return &obj
 }
